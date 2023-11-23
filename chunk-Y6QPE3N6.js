e16a9dc537f95c7a941d6804e8a22ea82cd47c1a
Mamadou Cisse <mciissee@gmail.com>
2023-11-23 13:26:46 +0100

    Auto-generated commit [ci skip]

--- chunk-Y6QPE3N6.js
+++ chunk-Y6QPE3N6.js
@@ -18,7 +18,7 @@ import{A as r,B as h}from"./chunk-3M42DFWL.js";import{Aa as c,R as o,Y as n,ka a
 
   | Package                                    | Description                                                  | Version                                                                                                                       |
   | ------------------------------------------ | ------------------------------------------------------------ | ----------------------------------------------------------------------------------------------------------------------------- |
-  | [\`@cisstech/nestjs-expand\`](/docs/nestjs-expand) | A NestJS module to build Dynamic Resource Expansion for APIs | [![version](https://img.shields.io/npm/v/@cisstech/nestjs-expand.svg)](https://www.npmjs.com/package/@cisstech/nestjs-expand) |
+  | [\`@cisstech/nestjs-expand\`](docs/nestjs-expand) | A NestJS module to build Dynamic Resource Expansion for APIs | [![version](https://img.shields.io/npm/v/@cisstech/nestjs-expand.svg)](https://www.npmjs.com/package/@cisstech/nestjs-expand) |
 
   ## \u{1F4C4} Docs
 
