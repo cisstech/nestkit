be37c3c781373910b6da2436c06425e6b67d7ea0
Mamadou Cisse <mciissee@gmail.com>
2023-11-23 13:13:01 +0100

    Auto-generated commit [ci skip]

---
+++ browser/chunk-X7SZS3QY.js
@@ -0,0 +1,70 @@
+import{A as r,B as h}from"./chunk-3M42DFWL.js";import{Aa as c,R as o,Y as n,ka as a,x as i}from"./chunk-3C5HI3CO.js";var p=(()=>{let t=class t{constructor(){this.markdown=`
+  <h1 align="center"> Nest KIT</h1>
+
+  <div align="center">
+
+  A collection of libraries for NestJS developers.
+
+  [![CI](https://github.com/cisstech/nestkit/actions/workflows/ci.yml/badge.svg)](https://github.com/cisstech/nestkit/actions/workflows/ci.yml)
+  [![codecov](https://codecov.io/gh/cisstech/nestkit/branch/main/graph/badge.svg)](https://codecov.io/gh/cisstech/nestkit)
+  [![codefactor](https://www.codefactor.io/repository/github/cisstech/nestkit/badge/main)](https://www.codefactor.io/repository/github/cisstech/nestkit/overview/main)
+  [![GitHub Tag](https://img.shields.io/github/tag/cisstech/nestkit.svg)](https://github.com/cisstech/nestkit/tags)
+  [![licence](https://img.shields.io/github/license/cisstech/nestkit)](https://github.com/cisstech/nestkit/blob/main/LICENSE)
+  [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+
+  </div>
+
+  ## Packages
+
+  | Package                                    | Description                                                  | Version                                                                                                                       |
+  | ------------------------------------------ | ------------------------------------------------------------ | ----------------------------------------------------------------------------------------------------------------------------- |
+  | [\`@cisstech/nestjs-expand\`](/docs/nestjs-expand) | A NestJS module to build Dynamic Resource Expansion for APIs | [![version](https://img.shields.io/npm/v/@cisstech/nestjs-expand.svg)](https://www.npmjs.com/package/@cisstech/nestjs-expand) |
+
+  ## \u{1F4C4} Docs
+
+  Documentation available at [https://cisstech.github.io/nest-kit/](https://cisstech.github.io/nest-kit/)
+
+  ## \u2328\uFE0F Development
+
+  - Clone and install
+
+  \`\`\`bash
+  git clone https://github.com/cisstech/nest-kit
+  cd nestkit
+  yarn
+  \`\`\`
+
+  - Serve demo
+
+  \`\`\`bash
+  yarn start
+  \`\`\`
+
+  Browser would open automatically at <http://localhost:4200/>.
+
+  ## \u{1F91D} Contribution
+
+  Contributions are always welcome. <br/>
+
+  Please read our [CONTRIBUTING.md](https://github.com/cisstech/nestkit/blob/main/CONTRIBUTING.md) first. You can submit any ideas as [pull requests](https://github.com/cisstech/nestkit/pulls) or as [GitHub issues](https://github.com/cisstech/nestkit/issues).
+
+  Please just make sure that ...
+
+  Your code style matches with the rest of the project
+
+  Unit tests pass
+
+  Linter passes
+
+  ## \u2753 Support Development
+
+  The use of this library is totally free.
+
+  As the owner and primary maintainer of this project, I am putting a lot of time and effort beside my job, my family and my private time to bring the best support I can by answering questions, addressing issues and improving the library to provide more and more features over time.
+
+  If this project has been useful, that it helped you or your business to save precious time, don't hesitate to give it a star to support its maintenance and future development.
+
+  ## \u2728 License
+
+  MIT \xA9 [Mamadou Cisse](https://github.com/cisstech)
+  `}};t.\u0275fac=function(e){return new(e||t)},t.\u0275cmp=i({type:t,selectors:[["app-home"]],standalone:!0,features:[a],decls:1,vars:1,consts:[[3,"data"]],template:function(e,d){e&1&&n(0,"nge-markdown",0),e&2&&o("data",d.markdown)},dependencies:[c,h,r],styles:["[_nghost-%COMP%]{display:block;width:100vw;height:100vh;padding:2rem 8rem;box-sizing:border-box}@media only screen and (max-width: 600px){[_nghost-%COMP%]{padding:2rem}}"],changeDetection:0});let s=t;return s})();var y=[{path:"",component:p}];export{y as default};
---
+++ browser/chunk-Y3WRG37J.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-NUBEWBEN.js";import"./chunk-3C5HI3CO.js";export{a as NGE_DOC_RENDERERS,b as NgeDocModule};
