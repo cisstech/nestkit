edd46f3e4cc8630ef098d87678baf8d351558983
Mamadou Cisse <mciissee@gmail.com>
2023-11-24 15:13:57 +0100

    test(expand): add more tests to expand service

--- libs/expand/src/lib/expand.service.spec.ts
+++ libs/expand/src/lib/expand.service.spec.ts
@@ -1,7 +1,83 @@
-import { DiscoveryService } from '@golevelup/nestjs-discovery'
+import {
+  DiscoveredClassWithMeta,
+  DiscoveredMethod,
+  DiscoveredMethodWithMeta,
+  DiscoveryService,
+} from '@golevelup/nestjs-discovery'
 import { Test, TestingModule } from '@nestjs/testing'
-import { EXPANDABLE_KEY, EXPANDER_KEY } from './expand'
+import {
+  DEFAULT_EXPAND_CONFIG,
+  EXPANDABLE_KEY,
+  EXPANDER_KEY,
+  EXPAND_CONFIG,
+  ExpandConfig,
+  ExpandContext,
+  ExpandableParams,
+} from './expand'
 import { ExpandService } from './expand.service'
+
+class InstructorDTO {
+  id!: number
+  name!: string
+  bio!: string
+}
+
+class CourseDTO {
+  id!: number
+  title!: string
+  description!: string
+  instructorId!: number
+  parentId?: number
+  relatedCourseIds?: number[]
+}
+
+type ExpandedCourseDTO = CourseDTO & {
+  instructor: InstructorDTO
+  parent?: CourseDTO
+  relatedCourses?: CourseDTO[]
+}
+
+const mockCourses: CourseDTO[] = [
+  {
+    id: 1,
+    title: 'Course 1',
+    description: 'Course 1 description',
+    instructorId: 1,
+  },
+  {
+    id: 2,
+    title: 'Course 2',
+    description: 'Course 2 description',
+    instructorId: 2,
+    parentId: 1,
+  },
+  {
+    id: 3,
+    title: 'Course 3',
+    description: 'Course 3 description',
+    instructorId: 3,
+    parentId: 1,
+    relatedCourseIds: [1, 2],
+  },
+]
+
+const mockInstructors: InstructorDTO[] = [
+  {
+    id: 1,
+    name: 'Instructor 1',
+    bio: 'Instructor 1 bio',
+  },
+  {
+    id: 2,
+    name: 'Instructor 2',
+    bio: 'Instructor 2 bio',
+  },
+  {
+    id: 3,
+    name: 'Instructor 3',
+    bio: 'Instructor 3 bio',
+  },
+]
 
 describe('ExpandService', () => {
   let expandService: ExpandService
@@ -19,6 +95,12 @@ describe('ExpandService', () => {
           provide: DiscoveryService,
           useValue: mockDiscoveryService,
         },
+        {
+          provide: EXPAND_CONFIG,
+          useValue: {
+            enableLogging: false,
+          } as ExpandConfig,
+        },
       ],
     }).compile()
 
@@ -33,17 +115,25 @@ describe('ExpandService', () => {
     expect(expandService).toBeDefined()
   })
 
+  it('should load config values', () => {
+    expect(expandService.config).toStrictEqual({
+      ...DEFAULT_EXPAND_CONFIG,
+      enableLogging: false,
+    })
+  })
+
   describe('onModuleInit', () => {
-    /*  it('should initialize expanders and handle missing providers', async () => {
-      // Mock data for testing
+    it('should initialize expanders and handle missing providers', async () => {
       const mockExpanders = [
-        { meta: 'expander1', discoveredClass: { instance: {} } },
-        { meta: 'expander2', discoveredClass: { instance: {} } },
-      ]
+        { meta: CourseDTO, discoveredClass: { instance: jest.fn() } },
+      ] as DiscoveredClassWithMeta<jest.Mock>[]
+
       const mockExpandables = [
-        { meta: { target: 'target1' }, discoveredMethod: { methodName: 'method1', parentClass: { name: 'Parent1' } } },
-        { meta: { target: 'target2' }, discoveredMethod: { methodName: 'method2', parentClass: { name: 'Parent2' } } },
-      ]
+        {
+          meta: { target: CourseDTO },
+          discoveredMethod: { methodName: 'getCourses', parentClass: { name: 'CourseController' } } as DiscoveredMethod,
+        },
+      ] as DiscoveredMethodWithMeta<ExpandableParams>[]
 
       // Mock the discovery service behavior
       mockDiscoveryService.providersWithMetaAtKey.mockResolvedValue(mockExpanders)
@@ -56,16 +146,16 @@ describe('ExpandService', () => {
       await expandService.onModuleInit()
 
       // Assertions
-      expect(mockDiscoveryService.providersWithMetaAtKey).toHaveBeenCalledWith('EXPANDER')
-      expect(mockDiscoveryService.methodsAndControllerMethodsWithMetaAtKey).toHaveBeenCalledWith('EXPANDABLE')
+      expect(mockDiscoveryService.providersWithMetaAtKey).toHaveBeenCalledWith(EXPANDER_KEY)
+      expect(mockDiscoveryService.methodsAndControllerMethodsWithMetaAtKey).toHaveBeenCalledWith(EXPANDABLE_KEY)
 
-      mockExpanders.forEach(expander => {
+      mockExpanders.forEach((expander) => {
         expect(expandService['expanders'].get(expander.meta)).toBe(expander.discoveredClass.instance)
       })
 
       expect(loggerSpy).not.toHaveBeenCalled() // No errors should be logged
     })
- */
+
     it('should handle missing providers and log an error', async () => {
       const mockExpandables = [
         {
@@ -86,6 +176,73 @@ describe('ExpandService', () => {
       expect(loggerSpy.mock.calls[0][0]).toBe(
         'Error during module initialization: Expand: missing providers with @RegisterExpander for : MyDTO used in MyController.missingMethod'
       )
+    })
+  })
+
+  describe('expand', () => {
+    const courseExpander = {
+      parent: jest.fn((context: ExpandContext<Request, CourseDTO>) => {
+        const { parent } = context
+        return parent.parentId ? mockCourses.find((c) => c.id === parent.parentId) : undefined
+      }),
+      instructor: jest.fn((context: ExpandContext<unknown, CourseDTO>) => {
+        return Promise.resolve(mockInstructors.find((i) => i.id === context.parent.instructorId)!)
+      }),
+      relatedCourses: jest.fn((context: ExpandContext<unknown, CourseDTO>) => {
+        return Promise.resolve(mockCourses.filter((c) => context.parent.relatedCourseIds?.includes(c.id)))
+      }),
+    }
+    courseExpander.parent.mockName('parent')
+    courseExpander.instructor.mockName('instructor')
+    courseExpander.relatedCourses.mockName('relatedCourses')
+
+    beforeEach(async () => {
+      const mockExpanders = [
+        { meta: CourseDTO, discoveredClass: { instance: courseExpander } },
+      ] as DiscoveredClassWithMeta<jest.Mock>[]
+
+      const mockExpandables = [
+        {
+          meta: { target: CourseDTO },
+          discoveredMethod: { methodName: 'getCourses', parentClass: { name: 'CourseController' } } as DiscoveredMethod,
+        },
+      ] as DiscoveredMethodWithMeta<ExpandableParams>[]
+
+      mockDiscoveryService.providersWithMetaAtKey.mockResolvedValue(mockExpanders)
+      mockDiscoveryService.methodsAndControllerMethodsWithMetaAtKey.mockResolvedValue(mockExpandables)
+
+      jest.spyOn(expandService, 'getMethodExpandableMetadata').mockImplementation((method) => {
+        return {
+          parent: { target: CourseDTO },
+        }[(method as jest.Mock).getMockName()]
+      })
+
+      await expandService.onModuleInit()
+    })
+
+    it('should expand a single entity', async () => {
+      const course = mockCourses[0]
+      const instructor = mockInstructors.find((i) => i.id === course.instructorId)
+
+      const result = await expandService.expand<ExpandedCourseDTO>({ query: { expands: ['instructor'] } }, course, {
+        target: CourseDTO,
+      })
+
+      expect(result.instructor).toStrictEqual(instructor)
+    })
+
+    it('should expand recursively', async () => {
+      const course = mockCourses.find((c) => c.parentId)!
+      const parent = mockInstructors.find((i) => i.id === course.parentId)!
+
+      const result = await expandService.expand<ExpandedCourseDTO>(
+        { query: { expands: ['parent', 'parent.instructor'] } },
+        course,
+        { target: CourseDTO }
+      )
+
+      expect(result.parent!.id).toStrictEqual(parent.id)
+      expect((result.parent as ExpandedCourseDTO).instructor).toBeDefined()
     })
   })
 })
--- libs/expand/src/lib/expand.service.ts
+++ libs/expand/src/lib/expand.service.ts
@@ -3,20 +3,34 @@
 
 import { DiscoveryService } from '@golevelup/nestjs-discovery'
 import { Inject, Injectable, Logger, OnModuleInit, Optional } from '@nestjs/common'
-import { EXPANDABLE_KEY, EXPANDER_KEY, EXPAND_CONFIG, ExpandConfig, ExpandMethod, ExpandableParams } from './expand'
-import { createObjectFromDotNotation } from './expand.utils'
+import {
+  DEFAULT_EXPAND_CONFIG,
+  EXPANDABLE_KEY,
+  EXPANDER_KEY,
+  EXPAND_CONFIG,
+  ExpandConfig,
+  ExpandMethod,
+  ExpandableParams,
+} from './expand'
+import { ExpansionThree, createExpansionThree } from './expand.utils'
 
 @Injectable()
 export class ExpandService implements OnModuleInit {
   private readonly logger = new Logger(ExpandService.name)
   private readonly expanders = new Map<Function, any>()
 
+  get config(): Readonly<ExpandConfig> {
+    return this.conf
+  }
+
   constructor(
     private readonly discovery: DiscoveryService,
     @Optional()
     @Inject(EXPAND_CONFIG)
-    private readonly config?: ExpandConfig
-  ) {}
+    readonly conf: ExpandConfig
+  ) {
+    this.conf = { ...DEFAULT_EXPAND_CONFIG, ...conf }
+  }
 
   /**
    * Lifecycle hook to be called once the module has been initialized.
@@ -48,7 +62,7 @@ export class ExpandService implements OnModuleInit {
         throw new Error('Expand: missing providers with @RegisterExpander for : ' + missing)
       }
 
-      if (this.config?.enableLogging) {
+      if (this.conf?.enableLogging) {
         this.logger.log('Expansion logging is enabled.')
       }
     } catch (error: any) {
@@ -65,75 +79,83 @@ export class ExpandService implements OnModuleInit {
    * @returns The expanded resource.
    * @throws Error if there's an issue during the expansion process.
    */
-  async expand(request: any, resource: any, params: ExpandableParams): Promise<any> {
-    try {
-      const root = params.rootField ? resource[params.rootField] : resource
-      if (!root) return resource
-
-      const { query } = request
-      if (!query.expands) return resource
-
-      const expander = this.expanders.get(params.target)
-      const properties =
-        typeof query.expands === 'object' && !Array.isArray(query.expands)
-          ? query.expands
-          : createObjectFromDotNotation(query.expands)
-
-      const recursiveExpand = async (parent: any) => {
-        for (const prop in properties) {
-          const method = expander[prop] as ExpandMethod
-          if (!method) {
-            this.logger.warn(`Expand: missing method ${prop} on ${params.target.name}`)
-            continue
-          }
-
-          let value = await method.call(expander, { parent, request })
-
-          const subProp = properties[prop]
-          if (value && typeof subProp === 'object') {
-            const recursive = Reflect.getMetadata(EXPANDABLE_KEY, method) as ExpandableParams
-            if (recursive) {
-              value = await this.expand(
-                {
-                  ...request,
-                  query: {
-                    ...request.query,
-                    expands: {
-                      ...subProp,
-                      ...Object.keys(properties).reduce(
-                        (acc, key) => ({ ...acc, [key]: true }),
-                        {} as Record<string, boolean>
-                      ),
-                    },
-                  },
-                },
-                value,
-                recursive
-              )
-            } else {
-              this.logger.warn(
-                `Expand: missing @Expandable on ${params.target.name}.${prop} to recursively expand ${Object.keys(
-                  subProp
-                )}`
-              )
-            }
-          }
-
-          parent[prop] = value
+  async expand<T = any>(request: any, resource: any, params: ExpandableParams): Promise<T> {
+    const recursive = async (request: any, resource: any, params: ExpandableParams, three: ExpansionThree) => {
+      try {
+        const root = params.rootField ? resource[params.rootField] : resource
+        if (!root) {
+          this.logger.log(`Expand: nothing to expand on ${params.target.name}`)
+          return resource
         }
-        return parent
+
+        const expander = this.expanders.get(params.target) as Record<string, ExpandMethod>
+        if (!expander) {
+          // This should never happen because of the check in onModuleInit so we just log a warning
+          this.logger.warn(`Expand: missing expander for ${params.target.name}`)
+          return resource
+        }
+
+        const resources = Array.isArray(root) ? root : [root]
+        const changes = await Promise.all(
+          resources.map(async (parent: any) => {
+            const extraValues: Record<string, unknown> = {}
+
+            for (const propName in three) {
+              const method = expander[propName]
+
+              if (!method) {
+                this.logger.warn(`Expand: missing method ${propName} on ${params.target.name}`)
+                continue
+              }
+
+              let value = await method.call(expander, { parent, request })
+
+              const propValue = three[propName]
+              if (value && typeof propValue === 'object') {
+                const recursiveParams = this.getMethodExpandableMetadata(method) as ExpandableParams
+                if (recursiveParams) {
+                  value = await recursive(request, value, recursiveParams, propValue)
+                } else {
+                  this.logger.warn(
+                    `Expand: missing @Expandable on ${
+                      params.target.name
+                    }.${propName} to recursively expand ${Object.keys(three)}`
+                  )
+                }
+              }
+
+              extraValues[propName] = value
+            }
+
+            return { ...parent, ...extraValues }
+          })
+        )
+
+        const result = Array.isArray(root) ? changes : changes[0]
+
+        return params.rootField ? { ...resource, [params.rootField]: result } : result
+      } catch (error: any) {
+        if (this.conf?.enableLogging) {
+          this.logger.error(`Error during expansion: ${error.message}`, error.stack)
+        }
+        throw error
       }
-
-      const changes = await Promise.all((Array.isArray(root) ? root : [root]).map(recursiveExpand))
-
-      const result = Array.isArray(root) ? changes : changes[0]
-
-      return params.rootField ? { ...resource, [params.rootField]: result } : result
-    } catch (error: any) {
-      if (this.config?.enableLogging) {
-        this.logger.error(`Error during expansion: ${error.message}`, error.stack)
-      }
-      throw error
     }
+
+    const { query } = request
+    const expands = query[this.conf.expandQueryParamName!]
+    if (!expands) return resource
+    return recursive(request, resource, params, createExpansionThree(expands))
+  }
+
+  /**
+   * Returns the @Expandable metadata for a given method using the Reflect API.
+   * @remarks
+   * This method is used internally as a wrapper around Reflect.getMetadata to make testing easier.
+   * @param target - The method to be inspected.
+   * @returns The @Expandable metadata for the given method or undefined if none is found.
+   */
+  getMethodExpandableMetadata(target: Function): ExpandableParams | undefined {
+    return Reflect.getMetadata(EXPANDABLE_KEY, target)
   }
 }
